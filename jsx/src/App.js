632ae1975af41146f59666a7972246c26649f928
Pedro Leite <pedroaug.fl@gmail.com>
2022-10-14 15:25:47 -0300

    refactored into classes and added three comments with props

--- jsx/src/App.js
+++ jsx/src/App.js
@@ -3,6 +3,24 @@ import CommentDetail from "./CommentDetail";
 import "./styles/tailwind.css";
 
 function App() {
-  return <CommentDetail />;
+  return (
+    <>
+      <CommentDetail
+        commentor="Dave"
+        date="13 November 2022, at 15:20"
+        text="That was great! ty"
+      />
+      <CommentDetail
+        commentor="_the_swinging_beast"
+        date="13 November 2022, at 15:55"
+        text="I kinda like it."
+      />
+      <CommentDetail
+        commentor="al dente master"
+        date="14 November 2022, at 10:33"
+        text="Something else for sure!."
+      />
+    </>
+  );
 }
 export default App;
--- jsx/src/CommentDetail.js
+++ jsx/src/CommentDetail.js
@@ -1,36 +1,35 @@
 import React from "react";
 import { faker } from "@faker-js/faker";
 
-function CommentDetail() {
-  return (
-    <div class="flex justify-center relative top-1/3">
-      <div class="relative grid grid-cols-1 gap-4 p-4 mb-8 border rounded-lg bg-white shadow-lg">
-        <div class="relative flex gap-4">
-          <img
-            src={faker.image.avatar()}
-            class="relative rounded-lg -top-8 -mb-4 bg-white border h-20 w-20"
-            alt=""
-            loading="lazy"
-          />
-          <div class="flex flex-col w-full">
-            <div class="flex flex-row justify-between">
-              <p class="relative text-xl whitespace-nowrap truncate overflow-hidden">
-                COMMENTOR
-              </p>
-              <a class="text-gray-500 text-xl" href="#">
-                <i class="fa-solid fa-trash"></i>
-              </a>
+class CommentDetail extends React.Component {
+  render() {
+    return (
+      <div class="flex justify-center relative top-1/3">
+        <div class="relative grid grid-cols-1 gap-4 p-4 mb-8 border rounded-lg bg-white shadow-lg">
+          <div class="relative flex gap-4">
+            <img
+              src={faker.image.avatar()}
+              class="relative rounded-lg -top-8 -mb-4 bg-white border h-20 w-20"
+              alt=""
+              loading="lazy"
+            />
+            <div class="flex flex-col w-full">
+              <div class="flex flex-row justify-between">
+                <p class="relative text-xl whitespace-nowrap truncate overflow-hidden">
+                  {this.props.commentor}
+                </p>
+                <a class="text-gray-500 text-xl" href="#">
+                  <i class="fa-solid fa-trash"></i>
+                </a>
+              </div>
+              <p class="text-gray-400 text-sm">{this.props.date}</p>
             </div>
-            <p class="text-gray-400 text-sm">20 April 2022, at 14:88 PM</p>
           </div>
+          <p class="-mt-4 text-gray-500">{this.props.text}</p>
         </div>
-        <p class="-mt-4 text-gray-500">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          <br /> Maxime quisquam vero adipisci beatae voluptas dolor ame.
-        </p>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default CommentDetail;
